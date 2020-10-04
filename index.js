a931623f7e7d6c8b13c8dba8042c866b6862231e
Losh531 <65097694+Losh531@users.noreply.github.com>
2020-10-04 10:27:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
-const puppeteer = require("puppeteer");
+// server.js
+// where your node app starts
+
+// we've started you off with Express (https://expressjs.com/)
+// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
+const express = require("express");
+const app = express();
+
+// our default array of dreams
+const dreams = [
+  "Find and count some sheep",
+  "Climb a really tall mountain",
+  "Wash the dishes"
+];
+
+// make all the files in 'public' available
+// https://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// https://expressjs.com/en/starter/basic-routing.html
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
+});
+app.get("/arc-sw.js", (request, response) => {
+  response.sendFile(__dirname + "/views/arc-sw.js");
+});
+const puppeteer = require('puppeteer');
 
 (async () => {
-  var i;
-  while (i < 50) {
-    const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
-    const page = await browser.newpage();
-    await page.goto("https://arcio-server.losh531.repl.co");
+  let i = 0;
+while (i < 50) { 
+  const browser = await puppeteer.launch({ args: ['--no-sandbox'] });;
+  const page = await browser.newPage();
+  await page.goto('https://arcio-server.losh531.repl.co');
+  await console.log("At Website")
+  i++;
+}
 
-    await console.log("at website");
-    i++;
-  }
-  //
-  //const express = require("express");
-  //const app = express();
 
-  // make all the files in 'public' available
-  // https://expressjs.com/en/starter/static-files.html
-  //app.use(express.static("public"));
-
-  // https://expressjs.com/en/starter/basic-routing.html
-  //app.get("/", (request, response) => {
-  // response.sendStatus(200);
-  //});
-  //const listener = app.listen(3000, () => {
-  //console.log("your app is listening on port " + listener.address().port);
-  //});
   //await browser.close();
 })();
+
+// listen for requests :)
+const listener = app.listen(80, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
